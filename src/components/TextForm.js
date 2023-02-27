52dee38fbad75633b490b230f3a2318fd658f4bf
Rahul <rg4005450@gmail.com>
2023-02-27 15:03:10 +0530

    Created about Page dark mode, light mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,30 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const [text,setText] = useState("Enter text here")
+    const [text, setText] = useState("")
 
     // text = "New state" //* Wrong way to change a state
     // setText = "new state" //* Right way
 
-    const handleUpClick = ()=>{
+    const handleUpClick = () => {
         // console.log("Clicked"+ text)
         let newText = text.toUpperCase();
         setText(newText)
     }
 
-    const handleOnChange = (event)=>{
+    const handleDownClick = () => {
+        // console.log("Clicked"+ text)
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const copyText = () => {
+        // console.log("Clicked"+ text)
+        copyBtn.innerText = "Copied to Clipboard!"
+        navigator.clipboard.writeText(text);
+        setTimeout(() => {
+            copyBtn.innerText = "Copy to Clipboard"
+        }, 2000)
+    }
+
+    const clearText = () => {
+        // console.log("Clicked"+ text)
+        setText("");
+    }
+
+    const handleOnChange = (event) => {
         // console.log("On Change")
         setText(event.target.value);
     }
 
     return (
-        <div>
-            <div className="mb-3">
-                <h1 className='my-3'>{props.heading}</h1>
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">Enter text Here</label>
-                <textarea className="form-control" value = {text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="5"></textarea>
+        <>
+            <div className='container'>
+                <div className="mb-3">
+                    <h1 className='my-3'>{props.heading}</h1>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="5" placeholder='Enter text here'></textarea>
+                </div>
+                <button type="button" className="btn btn-primary mx-2" onClick={handleUpClick}>UPPERCASE</button>
+                <button type="button" className="btn btn-primary mx-2" onClick={handleDownClick}>lowercase</button>
+                <button type="button" id="copyBtn" className="btn btn-primary mx-2" onClick={copyText}>Copy to Clipboard</button>
+                <button type="button" className="btn btn-danger" onClick={clearText}>Clear Text</button>
             </div>
-            <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-        </div>
+            <div className="container my-5">
+                <h1>Your Text Summary</h1>
+                <p><b>{text.split(" ").length - 1}</b> words, <b>{text.length}</b> Characters</p>
+                <p><b>{0.008 * text.split(" ").length}</b> Minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
